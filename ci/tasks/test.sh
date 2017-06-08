c17219244297fcea97503f29cf89035b88bccbfb
Dennis Leon <dleon@pivotal.io>
2017-06-08 10:58:11 -0700

    Update concourse files for new bosh-load-tests behavior
    - Bump bosh-load-tests submodule
    - Use prepare-config-server instead of scripts
    - Reduce number of instances in dynamic manifest because of worker
    performance limitations
    - Use array of static IPs
    
    [#144043827](https://www.pivotaltracker.com/story/show/144043827)
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -5,8 +5,6 @@ set -exu
 ROOT_DIR=$PWD
 
 
-cp -f ${PWD}/config-server-release/* /tmp/config-server-release
-
 case "$DB" in
   mysql)
     OUTER_CONTAINER_IP=$(set +eu; source /etc/profile.d/chruby.sh; chruby 2.3.1;
@@ -53,15 +51,12 @@ esac
 local_bosh_dir="/tmp/local-bosh/director"
 source "${local_bosh_dir}/env"
 
-# TODO: Can we clean up all these SSH keys? our ginkgo tests here probably aren't using them.
 bosh int /tmp/local-bosh/director/creds.yml --path /jumpbox_ssh/private_key > /tmp/jumpbox_ssh_key.pem
 chmod 400 /tmp/jumpbox_ssh_key.pem
 
 export BOSH_GW_PRIVATE_KEY="/tmp/jumpbox_ssh_key.pem"
 export BOSH_GW_USER="jumpbox"
 export BOSH_DIRECTOR_IP="10.245.0.3"
-
-bosh -n update-cloud-config $ROOT_DIR/bosh-load-tests-workspace/assets/cloud_config.yml -v network=director_network
 
 bosh upload-stemcell bosh-candidate-stemcell/bosh-stemcell-*.tgz
 
@@ -88,31 +83,6 @@ sed -i "s#BOSH_CLI_BIN#${BOSH_CLI_BIN}#g" $config_file_path
 
 sed -i s#PWD#${PWD}#g $config_file_path
 
-
-# PUT required parameters in config-server
-set +eu
-source /etc/profile.d/chruby.sh
-chruby 2.3.1
-yes y | gem install cf-uaac --no-document
-
-uaac  target https://${BOSH_DIRECTOR_IP}:8443 --skip-ssl-validation
-
-config_server_password=$(bosh int "${local_bosh_dir}/creds.yml" --path /director_config_server_client_secret)
-uaac token client get director_config_server -s ${config_server_password}
-
-cat <<EOF >payload.json
-{ "name": "/num_instances", "value": 10 }
-EOF
-uaac curl --insecure --request PUT --header Content-Type:Application/JSON --data "$(cat payload.json)" "https://${BOSH_DIRECTOR_IP}:8080/v1/data"
-
-# Stubbing out uaac, to prevent uaa starting and instead using uaa from our 'bosh uaa deployment'
-yes y| gem uninstall cf-uaac
-
-cat <<HERE >/usr/local/bin/uaac
-#!/usr/bin/env bash
-
-HERE
-chmod +x /usr/local/bin/uaac
-
+export CONFIG_SERVER_PASSWORD=$(bosh int "${local_bosh_dir}/creds.yml" --path /director_config_server_client_secret)
 
 go run bosh-load-tests-workspace/src/github.com/cloudfoundry-incubator/bosh-load-tests/main.go $config_file_path
